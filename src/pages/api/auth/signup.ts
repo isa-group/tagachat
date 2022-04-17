a7aa9b705939e2c79b7d6dff38914ea45bc2813c
Jose Sandoval <joseasandovalp@gmail.com>
2022-04-17 13:33:28 +0200

    feat: add signup api routes

---
+++ src/pages/api/auth/signup.ts
@@ -0,0 +1,48 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { connectToDatabase } from 'src/lib/mongodb'
+import { hashPassword } from 'src/lib/bcryptjs'
+
+type Data = {
+  message: string
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'POST') {
+    return res.status(500).json({ message: 'Route not valid' })
+  }
+
+  const { email, name, password } = req.body
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !name ||
+    !password ||
+    password.length < 6
+  ) {
+    return res.status(422).json({ message: 'Invalid Data' })
+  }
+
+  const { db } = await connectToDatabase()
+
+  const usersCollection = db.collection('users')
+
+  const user = await usersCollection.findOne({ email })
+
+  if (user) {
+    return res.status(422).json({ message: 'User already exists' })
+  }
+
+  const hashedPassword = await hashPassword(password)
+
+  await usersCollection.insertOne({
+    email,
+    name,
+    password: hashedPassword,
+  })
+
+  return res.status(201).json({ message: `User was created` })
+}
+
+export default handler
