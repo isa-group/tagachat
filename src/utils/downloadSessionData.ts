8e4032988be4543df165d2c1913138539b24144f
Jose Sandoval <joseasandovalp@gmail.com>
2022-06-08 17:01:03 +0200

    feat: first functional version

--- src/utils/downloadSessionData.ts
+++ src/utils/downloadSessionData.ts
@@ -1,24 +1,124 @@
+// @ts-nocheck
 import axios from 'axios'
 import { IMessage } from 'src/types/message.type'
 import { IRoom } from 'src/types/room.type'
+import { tagDTOptions, tagFIOptions } from './tagOptions'
+
+const tagsFI = tagFIOptions.reduce((acc, tag) => {
+  acc.set(tag, 0)
+  return acc
+}, new Map())
+
+const completeTags = tagDTOptions.reduce((acc, tag) => {
+  acc.set(tag, 0)
+  return acc
+}, tagsFI)
 
 function convertData(rooms: IRoom[]) {
-  let headers =
-    'session,room,participant,block,messageId,reviewer,tagFI,tagDT\n'
+  const headers =
+    [
+      'session',
+      'room',
+      'participant',
+      'block',
+      'reviewer',
+      'f',
+      'i',
+      's',
+      'u',
+      'd',
+      'su',
+      'ack',
+      'm',
+      'qyn',
+      'ayn',
+      'qwh',
+      'awh',
+      'fp',
+      'fnon',
+      'o',
+    ].join(',') + '\n'
+
+  const ultimateEpicData = []
 
   rooms.forEach((room: IRoom) => {
     const { sessionName, roomCode, messages } = room
 
-    messages.forEach((loopMessage: IMessage) => {
-      const { createdBy, block, id, tags } = loopMessage
+    const flattenedValues = messages
+      .map((loopMessage: IMessage) => {
+        const { createdBy, block, tags } = loopMessage
 
-      for (const reviewer in tags) {
-        headers += `${sessionName},${roomCode},${createdBy},${block},${id},${reviewer},${tags[reviewer].tagFI},${tags[reviewer].tagDT}\n`
+        if (tags) {
+          return Object.entries(tags).map(
+            ([reviewer, tags]) =>
+              new Map(
+                Object.entries({
+                  sessionName,
+                  roomCode,
+                  createdBy,
+                  block,
+                  reviewer,
+                  tagFI: tags.tagFI,
+                  tagDT: tags.tagDT,
+                  ...Object.fromEntries(completeTags),
+                })
+              )
+          )
+        }
+
+        return []
+      })
+      .flat()
+
+    const groupedByReviewer = flattenedValues.reduce((acc, curr) => {
+      const reviewer = curr.get('reviewer')
+      const createdBy = curr.get('createdBy')
+      const block = curr.get('block')
+
+      if (!acc[reviewer]) {
+        acc[reviewer] = {}
       }
+
+      if (!acc[reviewer][createdBy]) {
+        acc[reviewer][createdBy] = {}
+      }
+
+      if (!acc[reviewer][createdBy][block]) {
+        acc[reviewer][createdBy][block] = []
+      }
+
+      acc[reviewer][createdBy][block].push(curr)
+
+      return acc
+    }, {})
+
+    Object.values(groupedByReviewer).forEach((createdBy) => {
+      Object.values(createdBy).forEach((blocks) => {
+        Object.values(blocks).forEach((messages) => {
+          const tagCount = new Map()
+
+          messages.forEach((message) => {
+            const fi = message.get('tagFI')
+            const dt = message.get('tagDT')
+
+            if (fi) tagCount.set(fi, (tagCount.get(fi) || 0) + 1)
+            if (dt) tagCount.set(dt, (tagCount.get(dt) || 0) + 1)
+          })
+
+          const newMap = new Map([...messages[0], ...tagCount])
+          newMap.delete('tagFI')
+          newMap.delete('tagDT')
+
+          const mapIntoArray = Array.from(newMap.values())
+          const arrayIntoText = mapIntoArray.join(',')
+          ultimateEpicData.push(arrayIntoText)
+        })
+      })
     })
   })
 
-  return headers
+  const csv = headers + ultimateEpicData.join('\n')
+  return csv
 }
 
 export async function downloadSessionData(
@@ -33,7 +133,6 @@ export async function downloadSessionData(
     } = await axios.get(`/api/sessions/${sessionName}/rooms`)
 
     const csv = convertData(data)
-
     const blob = new Blob([csv], { type: 'text/csv' })
     const url = window.URL.createObjectURL(blob)
     const link = document.createElement('a')
