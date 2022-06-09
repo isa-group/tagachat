2d1141d762026c109612ca6ffc76907a22a15551
Jose Sandoval <joseasandovalp@gmail.com>
2022-06-09 12:42:38 +0200

    feat: add relative frequency to export data

--- src/utils/downloadSessionData.ts
+++ src/utils/downloadSessionData.ts
@@ -3,6 +3,45 @@ import { IMessage } from 'src/types/message.type'
 import { IRoom } from 'src/types/room.type'
 import { tagDTOptions, tagFIOptions } from './tagOptions'
 
+const headers =
+  [
+    'session',
+    'room',
+    'participant',
+    'block',
+    'reviewer',
+    'f',
+    'i',
+    's',
+    'u',
+    'd',
+    'su',
+    'ack',
+    'm',
+    'qyn',
+    'ayn',
+    'qwh',
+    'awh',
+    'fp',
+    'fnon',
+    'o',
+    'f_r',
+    'i_r',
+    's_r',
+    'u_r',
+    'd_r',
+    'su_r',
+    'ack_r',
+    'm_r',
+    'qyn_r',
+    'ayn_r',
+    'qwh_r',
+    'awh_r',
+    'fp_r',
+    'fnon_r',
+    'o_r',
+  ].join(',') + '\n'
+
 const tagsFI = tagFIOptions.reduce((acc, tag) => {
   acc.set(tag, 0)
   return acc
@@ -14,30 +53,6 @@ const completeTags = tagDTOptions.reduce((acc, tag) => {
 }, tagsFI)
 
 function convertData(rooms: IRoom[]) {
-  const headers =
-    [
-      'session',
-      'room',
-      'participant',
-      'block',
-      'reviewer',
-      'f',
-      'i',
-      's',
-      'u',
-      'd',
-      'su',
-      'ack',
-      'm',
-      'qyn',
-      'ayn',
-      'qwh',
-      'awh',
-      'fp',
-      'fnon',
-      'o',
-    ].join(',') + '\n'
-
   const taggedMessages: string[] = []
 
   rooms.forEach((room: IRoom) => {
@@ -72,37 +87,39 @@ function convertData(rooms: IRoom[]) {
     const groupedByReviewer = flattenedMessages.reduce(
       (acc, curr) => {
         const reviewer = curr.get('reviewer') as string
-        const createdBy = curr.get('createdBy') as number
         const block = curr.get('block') as number
+        const createdBy = curr.get('createdBy') as number
 
         if (!acc[reviewer]) {
           acc[reviewer] = {}
         }
 
-        if (!acc[reviewer][createdBy]) {
-          acc[reviewer][createdBy] = {}
+        if (!acc[reviewer][block]) {
+          acc[reviewer][block] = {}
         }
 
-        if (!acc[reviewer][createdBy][block]) {
-          acc[reviewer][createdBy][block] = []
+        if (!acc[reviewer][block][createdBy]) {
+          acc[reviewer][block][createdBy] = []
         }
 
-        acc[reviewer][createdBy][block].push(curr)
+        acc[reviewer][block][createdBy].push(curr)
 
         return acc
       },
       {} as {
         [reviewer: string]: {
-          [createdBy: number]: {
-            [block: number]: Map<string, string | number>[]
+          [block: number]: {
+            [createdBy: number]: Map<string, string | number>[]
           }
         }
       }
     )
 
-    Object.values(groupedByReviewer).forEach((createdBy) => {
-      Object.values(createdBy).forEach((blocks) => {
-        Object.values(blocks).forEach((messages) => {
+    Object.values(groupedByReviewer).forEach((block) => {
+      Object.values(block).forEach((createdBy) => {
+        const messagesByBlock: Map<any, any>[] = []
+
+        Object.values(createdBy).forEach((messages) => {
           const tagCount = new Map()
           messages.forEach((message) => {
             const fi = message.get('tagFI')
@@ -116,9 +133,36 @@ function convertData(rooms: IRoom[]) {
           taggedMessage.delete('tagFI')
           taggedMessage.delete('tagDT')
 
-          const mapIntoString = Array.from(taggedMessage.values()).join(',')
-          taggedMessages.push(mapIntoString)
+          messagesByBlock.push(taggedMessage)
         })
+
+        // getting sum of tags in each message
+        const totalTagCount = new Map()
+        messagesByBlock.forEach((message) => {
+          message.forEach((value, key) => {
+            if (tagFIOptions.includes(key) || tagDTOptions.includes(key)) {
+              totalTagCount.set(key, (totalTagCount.get(key) || 0) + value)
+            }
+          })
+        })
+
+        // adding relative frequency to each message
+        messagesByBlock.forEach((message) => {
+          message.forEach((value, key) => {
+            if (tagFIOptions.includes(key) || tagDTOptions.includes(key)) {
+              message.set(
+                `${key}_r`,
+                !totalTagCount.get(key) ? 0 : value / totalTagCount.get(key)
+              )
+            }
+          })
+        })
+
+        const finalMessage = messagesByBlock
+          .map((message) => Array.from(message.values()).join(','))
+          .join('\n')
+
+        taggedMessages.push(finalMessage)
       })
     })
   })
