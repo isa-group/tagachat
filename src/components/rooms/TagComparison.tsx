9781b98154917ccaecf496077dd3eaa8db60ba16
Jose Sandoval <joseasandovalp@gmail.com>
2022-06-10 20:55:28 +0200

    feat: add popover

--- src/components/rooms/TagComparison.tsx
+++ src/components/rooms/TagComparison.tsx
@@ -1,3 +1,17 @@
+import {
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react'
 import { CheckCircleIcon, WarningIcon } from '@chakra-ui/icons'
 
 type TagComparisonProps = {
@@ -10,18 +24,44 @@ type TagComparisonProps = {
 }
 
 const TagComparison = ({ tags }: TagComparisonProps) => {
-  console.log(tags)
-  const convertedTags = Object.values(tags).map((tag) => tag)
-  const sameTags = convertedTags.every(
-    (tag) =>
-      tag.tagFI === convertedTags[0].tagFI &&
-      tag.tagDT === convertedTags[0].tagDT
+  const onlyTags = Object.values(tags)
+  const sameTags = onlyTags.every(
+    (tag) => tag.tagFI === onlyTags[0].tagFI && tag.tagDT === onlyTags[0].tagDT
   )
 
-  return sameTags ? (
-    <CheckCircleIcon color="green.500" />
-  ) : (
-    <WarningIcon color="yellow.400" />
+  return (
+    <Popover trigger="hover" placement="left">
+      <PopoverTrigger>
+        {sameTags ? (
+          <CheckCircleIcon color="green.500" />
+        ) : (
+          <WarningIcon color="yellow.400" />
+        )}
+      </PopoverTrigger>
+      <PopoverContent>
+        <PopoverArrow />
+        <PopoverBody>
+          <TableContainer overflowX="hidden">
+            <Table size="sm">
+              <Thead>
+                <Th>Reviewer</Th>
+                <Th>FI</Th>
+                <Th>DT</Th>
+              </Thead>
+              <Tbody>
+                {Object.entries(tags).map(([userEmail, tags]) => (
+                  <Tr key={userEmail}>
+                    <Td>{userEmail}</Td>
+                    <Td>{tags.tagFI}</Td>
+                    <Td>{tags.tagDT}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
   )
 }
 
